7f49d9edb7e3d5bb79f0ab986c87c08e1379f6b4
rlcosta177 <rlcosta177@gmail.com>
2024-06-17 15:02:15 +0000

    all but ejbca

--- k8-deployment-files/run-kube.sh
+++ k8-deployment-files/run-kube.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-sleep 3
-kubectl apply -f postgres-deployment.yaml
-
-sleep 3
 kubectl apply -f plik-deployment.yaml
 
 sleep 3
@@ -13,11 +9,10 @@ sleep 3
 kubectl apply -f secret-deployment.yaml
 
 sleep 3
-kubectl apply -f ejbca-deployment.yaml
+kubectl apply -f cert-deployment.yaml
 
 sleep 3
 kubectl apply -f nginx-configmap.yaml
 
 sleep 3
 kubectl apply -f nginx-deployment.yaml
-
